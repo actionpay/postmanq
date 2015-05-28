4a2fc0ff6e13725743f885637e2197a7274585bf
byorty <byorty@mail.ru>
2015-05-28 17:52:34 +0300

    refactor log

---
+++ log/func.go
@@ -0,0 +1,57 @@
+package log
+
+import (
+	"fmt"
+	"runtime/debug"
+)
+
+// посылает сервису логирования запись для логирования произвольного уровня
+func log(message string, level Level, args ...interface{}) {
+	defer func(){recover()}()
+	// если уровень записи не ниже уровня сервиса логирования
+	// запись посылается сервису
+	if level <= level {
+		// если уровень выше "info", значит пишется ошибка
+		// добавляем к сообщению стек, чтобы посмотреть в чем дело
+		if level > InfoLevel && level == DebugLevel {
+			message = fmt.Sprint(message, "\n", string(debug.Stack()))
+		}
+		messages <- NewMessage(level, message, args...)
+	}
+}
+
+// пишет ошибку в лог
+func Err(message string, args ...interface{}) {
+	log(message, ErrorLevel, args...)
+}
+
+// пишет произвольную ошибку в лог и завершает программу
+func FailExit(message string, args ...interface{}) {
+	Err(message, args...)
+	//	app.Events() <- NewApplicationEvent(FinishApplicationEventKind)
+}
+
+// пишет системную ошибку в лог и завершает программу
+func FailExitWithErr(err error) {
+	FailExit("%v", err)
+}
+
+// пишет произвольное предупреждение
+func Warn(message string, args ...interface{}) {
+	log(message, WarningLevel, args...)
+}
+
+// пишет системное предупреждение
+func WarnWithErr(err error) {
+	Warn("%v", err)
+}
+
+// пишет информационное сообщение
+func Info(message string, args ...interface{}) {
+	log(message, InfoLevel, args...)
+}
+
+// пишет сообщение для отладки
+func Debug(message string, args ...interface{}) {
+	log(message, DebugLevel, args...)
+}
---
+++ log/writer.go
@@ -0,0 +1,80 @@
+package log
+
+import (
+	"os"
+	"path/filepath"
+	"fmt"
+	"time"
+)
+
+type Writer interface {
+	writeString(string)
+}
+
+type StdoutWriter struct {}
+
+func (this *StdoutWriter) writeString(str string) {
+	os.Stdout.WriteString(str)
+}
+
+type FileWriter struct {
+	filename string
+}
+
+func (this *FileWriter) writeString(str string) {
+	f, err := os.OpenFile(this.filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, os.ModePerm)
+	if err == nil {
+		_, err = f.WriteString(str)
+		f.Close()
+	}
+}
+
+type Writers []Writer
+
+func (w Writers) len() int {
+	return len(w)
+}
+
+func (w *Writers) init(service *Service) {
+	for i := 0;i < w.len();i++ {
+		if filenameRegex.MatchString(service.Output) { // проверяем получили ли из настроек имя файла
+			// получаем директорию, в которой лежит файл
+			dir := filepath.Dir(service.Output)
+			// смотрим, что она реально существует
+			if _, err := os.Stat(dir); os.IsNotExist(err) {
+				FailExit("directory %s is not exists", dir)
+			} else {
+				w.add(i, &FileWriter{service.Output})
+			}
+		} else if len(service.Output) == 0 || service.Output == "stdout" {
+			w.add(i, &StdoutWriter{})
+		}
+	}
+}
+
+func (w *Writers) add(i int, writer Writer) {
+	*w[i] = writer
+}
+
+func (w *Writers) write() {
+	for _, writer := range w {
+		go w.listenMessages(writer)
+	}
+}
+
+func (w *Writers) listenMessages(writer Writer) {
+	for message := range messages {
+		w.writeMessage(writer, message)
+	}
+}
+
+func (w *Writers) writeMessage(writer Writer, message *Message) {
+	writer.writeString(
+		fmt.Sprintf(
+			"PostmanQ | %v | %s: %s\n",
+			time.Now().Format("2006-01-02 15:04:05"),
+			logLevelById[message.Level],
+			fmt.Sprintf(message.Message, message.Args...),
+		),
+	)
+}
